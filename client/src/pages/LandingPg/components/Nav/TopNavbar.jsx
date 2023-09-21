1360abf418774fbe1571ec99a77644c9a6009bf4
psankhe28 <sankhepratiksha3@gmail.com>
2023-09-21 23:05:45 +0530

    Fixed google translate and navbar

--- client/src/pages/LandingPg/components/Nav/TopNavbar.jsx
+++ client/src/pages/LandingPg/components/Nav/TopNavbar.jsx
@@ -5,12 +5,15 @@ import { Link } from "react-scroll";
 import Sidebar from "../Nav/Sidebar";
 import Backdrop from "../Elements/Backdrop";
 // Assets
-import LogoIcon from "../../../../assets/logo.png";
 import BurgerIcon from "../../assets/svg/BurgerIcon";
+import LogoIcon from "../../../../assets/logo.png";
+import { useNavigate } from "react-router-dom";
+
 
 export default function TopNavbar() {
   const [y, setY] = useState(window.scrollY);
   const [sidebarOpen, toggleSidebar] = useState(false);
+  const navigate=useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => setY(window.scrollY));
@@ -19,90 +22,63 @@ export default function TopNavbar() {
     };
   }, [y]);
 
+  const submitHandler = async () => {
+    navigate("/login");
+  }
+  
+
 
   return (
     <>
       <Sidebar sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
       {sidebarOpen && <Backdrop toggleSidebar={toggleSidebar} />}
-      <Wrapper
-        className="flexCenter animate whiteBg"
-        style={y > 100 ? { height: "60px" } : { height: "80px" }}
-      >
+      <Wrapper className="flexCenter animate whiteBg" style={y > 100 ? { height: "60px" } : { height: "80px" }}>
         <NavInner className="container flexSpaceCenter">
-          <Link className="pointer flexNullCenter" to="home" smooth={true}>
+            <Link className="pointer flexNullCenter" to="home" smooth={true}>
             <img src={LogoIcon} alt="" width="80" height="10" />
+            
           </Link>
-          <BurderWrapper
-            className="pointer"
-            onClick={() => toggleSidebar(!sidebarOpen)}
-          >
+          <BurderWrapper className="pointer" onClick={() => toggleSidebar(!sidebarOpen)}>
             <BurgerIcon />
           </BurderWrapper>
           <UlWrapper className="flexNullCenter">
             <li className="semiBold font15 pointer">
-              <Link
-                activeClass="active"
-                style={{ padding: "10px 15px" }}
-                to="home"
-                spy={true}
-                smooth={true}
-                offset={-80}
-              >
+              <Link activeClass="active" style={{ padding: "10px 15px" }} to="home" spy={true} smooth={true} offset={-80}>
                 Home
               </Link>
             </li>
             <li className="semiBold font15 pointer">
-              <Link
-                activeClass="active"
-                style={{ padding: "10px 15px" }}
-                to="services"
-                spy={true}
-                smooth={true}
-                offset={-80}
-              >
-                Features
+              <Link activeClass="active" style={{ padding: "10px 15px" }} to="services" spy={true} smooth={true} offset={-80}>
+                Services
               </Link>
             </li>
-            {/* <li className="semiBold font15 pointer">
-              <Link activeClass="active" style={{ padding: "10px 15px" }} to="projects" spy={true} smooth={true} offset={-80}>
-                Projects
+            <li className="semiBold font15 pointer">
+              <Link activeClass="active" style={{ padding: "10px 15px" }} to="eligibility" spy={true} smooth={true} offset={-80}>
+              Eligibility
               </Link>
             </li>
             <li className="semiBold font15 pointer">
               <Link activeClass="active" style={{ padding: "10px 15px" }} to="blog" spy={true} smooth={true} offset={-80}>
                 Blog
               </Link>
+            </li>
+            {/* <li className="semiBold font15 pointer">
+              <Link activeClass="active" style={{ padding: "10px 15px" }} to="pricing" spy={true} smooth={true} offset={-80}>
+                Pricing
+              </Link>
             </li> */}
             <li className="semiBold font15 pointer">
-              <Link
-                activeClass="active"
-                style={{ padding: "10px 15px" }}
-                to="eligibility"
-                spy={true}
-                smooth={true}
-                offset={-80}
-              >
-                Eligibility Criteria
-              </Link>
-            </li>
-            <li className="semiBold font15 pointer">
-              <Link
-                activeClass="active"
-                style={{ padding: "10px 15px" }}
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={-80}
-              >
+              <Link activeClass="active" style={{ padding: "10px 15px" }} to="contact" spy={true} smooth={true} offset={-80}>
                 Contact
               </Link>
             </li>
           </UlWrapper>
           <UlWrapperRight className="flexNullCenter">
             <li className="semiBold font15 pointer">
-              <a href="/login" style={{ padding: "10px 30px 10px 0" }}>
+            <button onClick={() => submitHandler()}>Log in</button>
+              {/* <a href="/login" style={{ padding: "10px 30px 10px 0" }}>
                 Log in
-              </a>
+              </a> */}
             </li>
           </UlWrapperRight>
         </NavInner>
@@ -121,7 +97,7 @@ const Wrapper = styled.nav`
 const NavInner = styled.div`
   position: relative;
   height: 100%;
-`;
+`
 const BurderWrapper = styled.button`
   outline: none;
   border: 0px;
@@ -144,3 +120,4 @@ const UlWrapperRight = styled.ul`
     display: none;
   }
 `;
+
