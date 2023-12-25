f2fffd93a157aed8921d638fda87298a67d6643a
Chaitya Vora <90527884+vorachaitya@users.noreply.github.com>
2023-12-25 22:19:59 +0530

    hashing stored password

--- backend/config/generateHashedPassword.js
+++ backend/config/generateHashedPassword.js
@@ -9,3 +9,16 @@ const generateHashedPassword = async (password) => {
 };
 
 module.exports = generateHashedPassword;
+
+// const bcrypt = require("bcryptjs");
+
+// const generateHashedPassword = async (password) => {
+//   //  Hashing the password
+//   const salt = await bcrypt.genSaltSync(10);
+//   const hashedPassword = await bcrypt.hash(password, salt);
+
+//   return hashedPassword;
+// };
+
+// module.exports = generateHashedPassword;
+
